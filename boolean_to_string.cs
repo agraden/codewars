c61e472ccb00e3bf46d689fe6114cc1c5d7038a4
Alex <agraden@pm.me>
2024-01-31 19:06:31 -0500

    Update filenames

---
+++ boolean_to_string.cs
@@ -0,0 +1,11 @@
+// Title: "Convert a Boolean to a String"
+// 8 kyu
+using System;
+
+public class kata
+{
+    public static string BooleanToString(bool b)
+    {
+        return b.ToString();
+    }
+}
